342ab6dbc9c9ea31a5a10001c2689b52c289195c
tkthanatorn <tk.thanatorn@gmail.com>
2023-10-23 11:18:05 +0700

    impl: site region mapping repo

--- repo/site_region_mapping.go
+++ repo/site_region_mapping.go
@@ -2,11 +2,20 @@ package repo
 
 import (
 	"github.com/HavvokLab/true-solar-monitoring/model"
+	"github.com/HavvokLab/true-solar-monitoring/util"
 	"gorm.io/gorm"
 )
 
 type SiteRegionMappingRepo interface {
+	Count() (int64, error)
 	GetSiteRegionMappings() ([]model.SiteRegionMapping, error)
+	GetSiteRegionMappingsWithPagination(limit, offset int) ([]model.SiteRegionMapping, int64, error)
+	GetAreaNotNull() ([]model.SiteRegionMapping, error)
+	CreateCity(data *model.SiteRegionMapping) error
+	UpdateCity(id int64, data *model.SiteRegionMapping) error
+	DeleteCity(id int64) error
+	UpdateCityToNullArea(area string) error
+	UpdateSiteRegionMapping(codeListString, area string) error
 }
 
 type siteRegionMappingRepo struct {
@@ -17,13 +26,101 @@ func NewSiteRegionMappingRepo(db *gorm.DB) SiteRegionMappingRepo {
 	return &siteRegionMappingRepo{db: db}
 }
 
+func (r *siteRegionMappingRepo) Count() (int64, error) {
+	tx := r.db.Session(&gorm.Session{})
+	var count int64
+	err := tx.Model(&model.SiteRegionMapping{}).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
 func (r *siteRegionMappingRepo) GetSiteRegionMappings() ([]model.SiteRegionMapping, error) {
 	tx := r.db.Session(&gorm.Session{})
 	var siteRegionMappings []model.SiteRegionMapping
-	err := tx.Find(&siteRegionMappings).Error
+	err := tx.Find(&siteRegionMappings, "code NOT LIKE 'EMPTY-%'").Error
 	if err != nil {
-		return nil, err
+		return nil, util.TranslateSqliteError(err)
 	}
 
 	return siteRegionMappings, nil
+}
+
+func (r *siteRegionMappingRepo) GetSiteRegionMappingsWithPagination(limit, offset int) ([]model.SiteRegionMapping, int64, error) {
+	count, err := r.Count()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var siteRegionMappings []model.SiteRegionMapping
+	tx := r.db.Session(&gorm.Session{})
+	err = tx.Find(&siteRegionMappings, "code NOT LIKE 'EMPTY-%'").Limit(limit).Offset(offset).Error
+	if err != nil {
+		return nil, 0, util.TranslateSqliteError(err)
+	}
+
+	return siteRegionMappings, count, nil
+}
+
+func (r *siteRegionMappingRepo) GetAreaNotNull() ([]model.SiteRegionMapping, error) {
+	tx := r.db.Session(&gorm.Session{})
+	var siteRegionMappings []model.SiteRegionMapping
+	err := tx.Find(&siteRegionMappings, "code LIKE 'EMPTY-%' AND area NOT NULL").Error
+	if err != nil {
+		return nil, util.TranslateSqliteError(err)
+	}
+
+	return siteRegionMappings, nil
+}
+
+func (r *siteRegionMappingRepo) CreateCity(data *model.SiteRegionMapping) error {
+	tx := r.db.Session(&gorm.Session{})
+	err := tx.Create(data).Error
+	if err != nil {
+		return util.TranslateSqliteError(err)
+	}
+
+	return nil
+}
+
+func (r *siteRegionMappingRepo) UpdateCity(id int64, data *model.SiteRegionMapping) error {
+	tx := r.db.Session(&gorm.Session{})
+	err := tx.Model(&model.SiteRegionMapping{}).Where("id = ?", id).Updates(data).Error
+	if err != nil {
+		return util.TranslateSqliteError(err)
+	}
+
+	return nil
+}
+
+func (r *siteRegionMappingRepo) DeleteCity(id int64) error {
+	tx := r.db.Session(&gorm.Session{})
+	err := tx.Delete(&model.SiteRegionMapping{}, id).Error
+	if err != nil {
+		return util.TranslateSqliteError(err)
+	}
+
+	return nil
+}
+
+func (r *siteRegionMappingRepo) UpdateCityToNullArea(area string) error {
+	tx := r.db.Session(&gorm.Session{})
+	err := tx.Model(&model.SiteRegionMapping{}).Where("area = ?", area).Update("area", nil).Error
+	if err != nil {
+		return util.TranslateSqliteError(err)
+	}
+
+	return nil
+}
+
+func (r *siteRegionMappingRepo) UpdateSiteRegionMapping(codeListString, area string) error {
+	tx := r.db.Session(&gorm.Session{})
+	err := tx.Model(&model.SiteRegionMapping{}).Where("code IN ?", codeListString).Update("area", area).Error
+	if err != nil {
+		return util.TranslateSqliteError(err)
+	}
+
+	return nil
 }
